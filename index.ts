e1ca59ae0737ab9c7b3a9f31f4425d5fde4d9110
dimakovalev25 <d.nozdryn@gmail.com>
2022-12-28 23:45:25 +0300

    TS firs step

---
+++ index.ts
@@ -0,0 +1,15 @@
+// ts-node index.ts
+
+const str = 'str';
+// console.log(str);
+
+let userName: string = 'ivan';
+// userName = 1;
+
+
+let y: boolean = true;
+let num: number = 5;
+const names: string = 'ivan';
+if (y) {
+    console.log(`${names}: ${num + 1}`);
+}
