512cb8ec916da04f5a1328f968f90e3f1cea1b84
dimakovalev25 <d.nozdryn@gmail.com>
2022-12-29 20:32:11 +0300

    типизация объектов и массивов

--- index.ts
+++ index.ts
@@ -1,4 +1,51 @@
 // ts-node index.ts
+
+const userData = {
+    isData: true,
+    age: 40,
+    name: 'John',
+}
+
+// function Brt(isData: boolean, age: number, name: string): string {
+//     if(isData === true) {
+//         return `Congratulations ${name} `
+//     } else {
+//         return `Sorry bro!`
+//     }
+// }
+
+//типизация объектов!
+
+function Brtz(data: {
+    isData: boolean,
+    age: number,
+    name: string
+
+}): string {
+    if (data.isData) {
+        return `Congratulations ${data.name.toUpperCase()}!!`
+    } else {
+        return `Sorry bro!`
+    }
+}
+
+// Brtz(userData);
+// console.log(Brtz(userData));
+
+
+//типизация массивов!
+
+
+const dep: string[] = ['dev', 'design', 'mark'];
+const num: number[] = [1, 2];
+const n: any[] = [1, 2,'f'];
+//массив массивов матрица
+const number: number[][] = [[1, 2],[1]];
+// dep.push(1);
+dep.push('f');
+console.log(dep);
+
+
 // let yData: boolean = true;
 // let numData: number = 5;
 // const namesData: string = 'ivan';
@@ -36,9 +83,44 @@
 
 // transferEurToUsd(true, 500, 1.05);
 
-let id: symbol = Symbol('id');
 
-const data = {
-    [id]: 1
-}
-console.log(data);
+
+
+const electricityUserData = {
+    readings: 95,
+    units: "kWt",
+    mode: "double",
+};
+
+const waterUserData = {
+    readings: 3,
+    units: "m3",
+};
+
+const elRate = 0.45;
+const wRate = 2;
+
+const monthPayments = [0, 0]; // [electricity, water]
+
+const calculatePayments = (elData, wData, elRate, wRate) => {
+    if (elData.mode === "double" && elData.readings < 50) {
+        monthPayments[0] = elData.readings * elRate * 0.7;
+    } else {
+        monthPayments[0] = elData.readings * elRate;
+    }
+
+    monthPayments[1] = wData.readings * wRate;
+};
+
+calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+
+const sendInvoice = (monthPayments, electricityUserData, waterUserData) => {
+    const text = `    Hello!
+    This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
+    It will cost: ${monthPayments[0]}€
+    
+    This month you used ${waterUserData.readings} ${waterUserData.units} of water
+    It will cost: ${monthPayments[1]}€`;
+
+    return text;
+};
